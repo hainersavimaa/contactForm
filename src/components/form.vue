9e0230db4d5cd0cba05c838371b21aa722212806
Hainer Savimaa <hainer.savimaa@gmail.com>
2017-11-20 21:45:25 +0200

    Lisatud kommentaarid

--- src/components/form.vue
+++ src/components/form.vue
@@ -3,20 +3,22 @@
       <form id="contactForm" class="contact-form" v-if="!submitted">
          <h1>Kontaktandmed</h1>
          <label class="field-label" for="">Ees -ja perenimi</label>
-         <input type="text" name="Nimi" v-model="contactForm.submittedName" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('Nimi') }"/>
-         <span v-show="errors.has('Nimi')" class="help is-danger">{{ errors.first('Nimi') }}</span>
+         <input type="text" name="Nimi" v-model="contactForm.submittedName" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('Nimi') }"/> <!-- Andmed väljast loetakse contactForm vastavasse muutujasse. Lisatud on if-lause, kui on probleem valideerimisel, lisatakse vastav klass. -->
+         <span v-show="errors.has('Nimi')" class="help is-danger">{{ errors.first('Nimi') }}</span> <!-- Veateate tekst, mis ilmub, kui v-validate avastab vea. -->
          <br>
          <label class="field-label" for="">Telefon</label>
          <input type="tel" id="phone-input" name="Telefon" v-model="contactForm.submittedPhone" v-validate="phoneRules" :class="{'input': true, 'is-danger': errors.has('Telefon') }" placeholder="+372 "/>
-         <span v-show="errors.has('Telefon')" class="help is-danger">{{ errors.first('Telefon') }}</span>
+         <!-- Andmed väljast loetakse contactForm vastavasse muutujasse. Lisatud on if-lause, kui on probleem valideerimisel, lisatakse vastav klass. Lisatud on custom reegel, mis kontrollib, kas e-mail on sisestatud. -->
+         <span v-show="errors.has('Telefon')" class="help is-danger">{{ errors.first('Telefon') }}</span> <!-- Veateate tekst, mis ilmub, kui v-validate avastab vea. -->
          <br>
          <label class="field-label" for="">E-post</label>
          <input type="email" name="Email" v-model="contactForm.submittedEmail" v-validate="emailRules" :class="{'input': true, 'is-danger': errors.has('Email') }"/>
-         <span v-show="errors.has('Email')" class="help is-danger">{{ errors.first('Email') }}</span>
+                  <!-- Andmed väljast loetakse contactForm vastavasse muutujasse. Lisatud on if-lause, kui on probleem valideerimisel, lisatakse vastav klass. Lisatud on custom reegel, mis kontrollib, kas telefoninumber on sisestatud. -->
+         <span v-show="errors.has('Email')" class="help is-danger">{{ errors.first('Email') }}</span> <!-- Veateate tekst, mis ilmub, kui v-validate avastab vea. -->
          <br>
          <label class="field-label" for="">Tänav, maja, korter</label>
          <input type="text" name="Aadress" v-model="contactForm.submittedAddress" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('Aadress') }"/>
-         <span v-show="errors.has('Aadress')" class="help is-danger">{{ errors.first('Aadress') }}</span>
+         <span v-show="errors.has('Aadress')" class="help is-danger">{{ errors.first('Aadress') }}</span> <!-- Veateate tekst, mis ilmub, kui v-validate avastab vea. -->
          <br>
          <label class="field-label" for="">Küla/alevik</label>
          <input type="text" name="Alevik" v-model="contactForm.submittedArea"/>
@@ -40,7 +42,7 @@
             <option value="Viljandimaa">Viljandimaa</option>
             <option value="Võrumaa">Võrumaa</option>
          </select>
-         <span v-show="errors.has('Maakond')" class="help is-danger">{{ errors.first('Maakond') }}</span>
+         <span v-show="errors.has('Maakond')" class="help is-danger">{{ errors.first('Maakond') }}</span> <!-- Veateate tekst, mis ilmub, kui v-validate avastab vea. -->
          <br>
          <div class="radio-button-wrap">
             <input type="radio" name="deliveryAddress" id="delivery1" value="false" checked v-model="contactForm.newAddress"/> <label for="delivery1" class="check-label">Kohaletoimetamine sama aadressile</label>
@@ -52,7 +54,7 @@
          <div class="additional-form" v-if="contactForm.newAddress==='true'">
             <label class="field-label" for="">Tänav, maja, korter</label>
             <input type="text" name="Lisa-aadress" v-model="contactForm.submittedExtraAddress" v-validate="'required'" :class="{'input': true, 'is-danger': errors.has('Lisa-aadress') }"/>
-            <span v-show="errors.has('Lisa-aadress')" class="help is-danger">{{ errors.first('Lisa-aadress') }}</span>
+            <span v-show="errors.has('Lisa-aadress')" class="help is-danger">{{ errors.first('Lisa-aadress') }}</span> <!-- Veateate tekst, mis ilmub, kui v-validate avastab vea. -->
             <br>
             <label class="field-label" for="">Küla/alevik</label>
             <input type="text" name="Lisa-Alevik" v-model="contactForm.submittedExtraArea"/>
@@ -76,7 +78,7 @@
                <option value="Viljandimaa">Viljandimaa</option>
                <option value="Võrumaa">Võrumaa</option>
             </select>
-            <span v-show="errors.has('Linn')" class="help is-danger">{{ errors.first('Linn') }}</span>
+            <span v-show="errors.has('Linn')" class="help is-danger">{{ errors.first('Linn') }}</span> <!-- Veateate tekst, mis ilmub, kui v-validate avastab vea. -->
          </div> <!-- Lisa-aadressi lõpp -->
          <div class="accept-box-wrap">
             <input type="checkbox" value="true" name="acceptCond" id="acceptCond" v-model="contactForm.submittedAccept"/> <!-- v-model lisab true väärtuse submittedAccept muutujale -->
@@ -102,10 +104,10 @@
 <script>
 export default {
    computed: {
-    emailRules() {
+    emailRules() {   // Valideerimisreegel e-post lahtrile, mis kontrollib, kas telefoninumber on sisestatud
       return this.contactForm.submittedPhone.length ? '' : 'required|email';
    },
-   phoneRules() {
+   phoneRules() { // Valideerimisreegel telefoninumber lahtrile, mis kontrollib, kas e-mailiaadress on sisestatud
      return this.contactForm.submittedEmail.length ? '' : 'required|numeric';
    }
 },
@@ -131,11 +133,11 @@ export default {
       submitForm: function(){ // See funktsioon kutsutakse "Salvesta" nuppu vajutades.
         this.$validator.validateAll().then((result) => { //vee-validate funktsioon, mis valideerib kõik required väljad.
         if (result) {   // kui on valideeritud
-          alert('Vorm salvestatud');
+          //alert('Vorm salvestatud');
           this.submitted = true; // submitted on true. st et kuvatakse readOnly vaade
           return;
         }
-        alert('Palun korrasta väljad!');
+        //alert('Palun korrasta väljad!');
       });
          //this.submitted = true; // Kuvatakse readOnly vaade ja peidetakse kontaktvorm
          console.log(this.contactForm.newAddress);
@@ -146,17 +148,14 @@ export default {
    }
 }
 </script>
+
 <style lang="scss" scoped>
-/* Muutujad */
+/* Muutujad. Muuda, et kiiresti re-brändida. */
 $main: #8470E5;
 $secondary: #13B2BB;
 $input-bg: #ccc;
 $borders: #ddd;
-
 $font: 'Lato', sans-serif;
-
-
-
 
 
 /* CLEARFIX */
